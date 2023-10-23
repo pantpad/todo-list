3726b339a8a648044523a0a4d8bf6be4ae51dfdb
pantpad <138708449+pantpad@users.noreply.github.com>
2023-10-23 17:02:09 +0200

    Add listener for a new project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('come va?');
+
+const addProject = document.querySelector('.add-project');
+
+addProject.addEventListener('click',(e) => {
+    e.preventDefault;
+    console.log(e.target);
+});

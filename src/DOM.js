5516162c21d7c3f35a23191e0468cde14cd9a362
pantpad <138708449+pantpad@users.noreply.github.com>
2023-10-26 11:43:02 +0200

    refactor imports

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,4 @@
-import projects from "./projects.js";
+import projects from "./projects";
 
 const DOM = (function () {
 
--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-import DOM from "./DOM.js";
+import DOM from "./DOM";
 
 const projects = (() => {
 
